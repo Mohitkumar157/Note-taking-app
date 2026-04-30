28bc16ed50af338a6473a9662101cbb5297d3e3b
Mohit Kumar <mohitkuma8818@gmail.com>
2026-05-01 02:27:01 +0530

    fix env variable final

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URL
+const MONGODB_URI = process.env.MONGODB_URI
 
 let isConnected = false;
 export default async function connectDB() {
+    if (!MONGODB_URI) {
+        console.log("❌ ENV:", MONGODB_URI);
+        throw new Error("MONGODB_URI missing");
+    }
     if (isConnected) {
         console.log("already connected to database");
         return
